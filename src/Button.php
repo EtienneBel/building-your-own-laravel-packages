d69f9c40e38ddf48a79e20942a1e620daa79d77b
Wend-Kuuni <belemgnegreetienne@gmail.com>
2024-10-22 20:30:03 +0000

    Refactor Button component to update view path

--- src/Button.php
+++ src/Button.php
@@ -79,7 +79,7 @@ class Button extends Component
         $baseClasses = 'inline-flex items-center justify-center font-medium rounded-md transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed';
         $classes = $baseClasses . ' ' . $this->getVariantClasses() . ' ' . $this->getSizeClasses();
 
-        return view('tailwind-buttons::button', [
+        return view('livewire-buttons::button', [
             'classes' => $classes
         ]);
     }
